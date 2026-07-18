157d87f89537767177c9c03de9d4e814deee8d49
niranjanathirala16-svg <niranjanathirala16@gmail.com>
2026-07-18 11:04:38 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+
+import joblib
+
+
+app = Flask(__name__)
+
+# Load trained Random Forest model
+model = joblib.load("heart_model.pkl")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    features = [
+        float(request.form["id"]),
+        float(request.form["age"]),
+        float(request.form["gender"]),
+        float(request.form["height"]),
+        float(request.form["weight"]),
+        float(request.form["ap_hi"]),
+        float(request.form["ap_lo"]),
+        float(request.form["cholesterol"]),
+        float(request.form["glucose"]),
+        float(request.form["smoke"]),
+        float(request.form["alco"]),
+        float(request.form["active"])
+    ]
+
+    prediction = model.predict([features])
+
+    if prediction[0] == 1:
+        result = "Heart disease detected"
+    else:
+        result = "No heart disease detected"
+
+    return render_template("index.html", prediction=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
--- heart_disease.py
+++ heart_disease.py
@@ -191,12 +191,16 @@ print(confusion_matrix(y_test, y_pred_rf))
 print("\nclassification report=====")
 print(classification_report(y_test,y_pred_rf))
 
-
 import joblib
 
-joblib.dump(rf_model, "heart_model.pkl")
 
-print("Model saved successfully!")
+joblib.dump(rf_model, "heart_model.pkl")
+print("Random forest model saved succesfully!")
+
+
+
+
+
 
 #SUPPORT VECTOR MECHINE
 
